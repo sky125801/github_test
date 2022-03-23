4ecee53eb15303e373de207852af18657964f4a0
sky125801 <mushanps@sina.com>
2022-03-23 13:40:17 +0900

    Add app scrpt by vue

---
+++ App.vue
@@ -0,0 +1,27 @@
+<template>
+
+  <ExampleDemo flag=false isActive=false hasError=true activeClass='active' errorClass='error'></ExampleDemo>
+  
+</template>
+
+<script>
+import ExampleDemo from './components/example3-6.vue'
+
+export default {
+  name: 'App',
+  components: {
+    'ExampleDemo': ExampleDemo
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
